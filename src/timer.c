352c2a887cc04942b0135816db174251a66e1d94
Connor <cdubthecoolcat@gmail.com>
2020-01-19 13:38:12 -0800

    remove not needed floor

--- src/timer.c
+++ src/timer.c
@@ -1,12 +1,11 @@
 #include "timer.h"
-#include <math.h>
 #include <time.h>
 
 const struct timespec generate_delay(long double delay) {
   unsigned seconds = 0;
   unsigned nanoseconds = 0;
 
-  seconds = (int)floor(delay);
+  seconds = (int) delay;
   nanoseconds = (delay - seconds) * NANO_MULTI;
   return (const struct timespec){seconds, nanoseconds};
 }
